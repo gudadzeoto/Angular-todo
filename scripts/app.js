daddad47b428a1b9cacb996fb2508110e04fe80a
Gudadze Oto <otogudadze95@gmail.com>
2019-10-08 21:14:26 +0400

    Add actions controller.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
-angular.module("todoApp", []);
+angular.module("todoApp", [])
+.controller("mainCtrl", function($scope) {
+	$scope.helloWorld = function() {
+		console.log("Senaxulia");
+	};
+})
+.controller("actionsCtrl", function($scope) {
+	$scope.edit = function() {
+		console.log("edit");
+	};
+	$scope.save = function() {
+		console.log("save");
+	};
+	$scope.delete = function() {
+		console.log("delete");
+	};
+});
